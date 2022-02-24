670da9e84a6d7aa0b5c9653c01a6961e36f03a0e
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-24 18:12:34 +0100

    Add schema logico

---
+++ dadi/js/main.js
@@ -0,0 +1,7 @@
+// Genero numero a caso per utente 
+// genero num random per pc
+// confronto num utente e num pc
+// --se num utente > num pc
+    // -hai vinto
+// --se num utente< num pc 
+    // -haiperso
