b68732d7a0fa9c7ef0392f51000409db55d10677
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-24 15:53:28 +0100

    First try

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+const emailArr = ['juni4@gmail.com', 'junaid4567@gmail.com', 'juni1@gmail.com', 'juni1@yahoo.com', 'juni2@yahoo.com', 'juni3@yahoo.com', 'juni2@rocketmail.com', 'juni3@rocketmail.com', 'juni1@rocketmail.com', 'juni2@ymail.com', 'juni3@ymail.com', 'juni1@ymail.com'];
 
+let userEmail = prompt("Inserisci la tua email!");
+
+if(userEmail = emailArr){
+    console.log("Email trovata");
+}else{
+    console.log("Email non trovata");
+}
