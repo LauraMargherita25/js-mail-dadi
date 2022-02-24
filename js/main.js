71c754f72b10fed4b0cd35c9e9a24a41a34b1ee6
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-24 17:39:04 +0100

    It's working

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
 const emailArr = ['juni4@gmail.com', 'junaid4567@gmail.com', 'juni1@gmail.com', 'juni1@yahoo.com', 'juni2@yahoo.com', 'juni3@yahoo.com', 'juni2@rocketmail.com', 'juni3@rocketmail.com', 'juni1@rocketmail.com', 'juni2@ymail.com', 'juni3@ymail.com', 'juni1@ymail.com'];
 
-let userEmail = prompt("Inserisci la tua email!");
+const userEmail = prompt("Inserisci la tua email!");
 
-if(userEmail = emailArr){
+let found = false;
+
+for (let i = 0; i < emailArr.length; i++) {
+    
+    if(emailArr[i] == userEmail){
+        found = true;
+    }
+}
+
+if(found){
     console.log("Email trovata");
 }else{
     console.log("Email non trovata");
 }
+
+// if(userEmail == emailArr){
+//     console.log("Email trovata");
+// }else{
+//     console.log("Email non trovata");
+// }
+
+
+/*
+
+- chiedere email all'utente
+- controllare ogni email nell'array
+    - se trovo email a cui sono arrivato == a quella dell'utente me lo segno
+- se dove mi sono segnato l'informazione di prima c'è scritto che la mail è stata trovata
+    - lo scrivo
+    - altrimenti scrivo il contrario
+
+*/
