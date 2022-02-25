72175b736c1357a15656ee8c26cfd89af36967d5
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-25 01:28:43 +0100

    Trying to add all element trough js failed

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -6,14 +6,86 @@
 // --se num utente< num pc 
     // -haiperso
 
+let mainContainer = document.querySelector(".main_container");
 
-let userNumber = Math.floor(Math.random() * 6) +1;
-let computerNumber = Math.floor(Math.random() * 6) +1;
 
-console.log(userNumber);
 
-if( userNumber > computerNumber){
-    console.log("Hai vinto!");
-}else if ( userNumber < computerNumber){
-    console.log("Hai perso!");
-}
+const btnDice = document.createElement("button");
+btnDice.classList.add("btn_dice");
+btnDice.innerHTML = ("Roll dice");
+mainContainer.append(btnDice);
+
+btnDice.addEventListener("click", function (){
+    let randomNumberPc = Math.floor(Math.random() * 6) + 1;
+    let randomNumberUser = Math.floor(Math.random() * 6) + 1; 
+})
+
+    
+/*const boxRed = document.createElement("div");
+boxRed.classList.add("box", "red");
+boxRed.innerHTML = randomNumberPc;
+mainContainer.append(boxRed);*/
+
+const boxGreen = document.createElement("div");
+boxGreen.classList.add("box", "green");
+boxGreen.innerHTML = randomNumberUser;
+mainContainer.append(boxGreen);
+
+
+
+/*btnOpenMenu.addEventListener("click", function(){
+    hiddenMenu.classList.add("active");
+//     hiddenMenu.style.display = "block";
+})*/
+
+// let computerNumber = Math.floor(Math.random() * 6) +1;
+
+// if( userNumber > computerNumber){
+//     console.log("Hai vinto!");
+
+// }else if ( userNumber < computerNumber){
+//     console.log("Hai perso!");
+// }
+
+
+
+
+
+
+
+// const box = document.createElement("div");
+// box.classList.add("box", "red");
+// box.innerHTML = userNumber;
+// mainContainer.append(box);
+
+// const box = document.createElement("div");
+// box.classList.add("box", "green");
+// box.innerHTML = computerNumber;
+// mainContainer.append(box);
+
+
+
+
+
+/*for (let i = 1; i <= 100; i++) {
+
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.innerHTML = i;
+
+    if(i % 3 == 0 && i % 5 == 0){
+        // itemContainer.innerHTML += `<div class="box blue">fizzbuzz</div>`;
+        box.classList.add("blue")
+        box.innerHTML = ("fizzbuzz");
+    }else if (i % 5 == 0){
+        // itemContainer.innerHTML += `<div class="box green">buzz</div>`;
+        box.classList.add("green")
+        box.innerHTML = ("buzz");
+    }else if(i % 3 == 0){
+        // itemContainer.innerHTML += `<div class="box red">fizz</div>`;
+        box.classList.add("red")
+        box.innerHTML = ("fiz");
+    }
+
+    itemContainer.append(box);
+}*/
--- mail/js/main.js
+++ mail/js/main.js
@@ -1,21 +1,26 @@
 const emailArr = ['juni4@gmail.com', 'junaid4567@gmail.com', 'juni1@gmail.com', 'juni1@yahoo.com', 'juni2@yahoo.com', 'juni3@yahoo.com', 'juni2@rocketmail.com', 'juni3@rocketmail.com', 'juni1@rocketmail.com', 'juni2@ymail.com', 'juni3@ymail.com', 'juni1@ymail.com'];
 
-const userEmail = prompt("Inserisci la tua email!");
+const userEmail = document.getElementById("#user_email");
 
-let found = false;
+const btnCheck = document.getElementById("#btn_check");
 
-for (let i = 0; i < emailArr.length; i++) {
+btnCheck.addEventListener("click", function(){    
+    let found = false;
     
-    if(emailArr[i] == userEmail){
-        found = true;
+    for (let i = 0; i < emailArr.length; i++) {
+        
+        if(emailArr[i] == userEmail){
+            found = true;
+        }
     }
-}
+    
+    if(found){
+        console.log("Email trovata");
+    }else{
+        console.log("Email non trovata");
+    }
+})
 
-if(found){
-    console.log("Email trovata");
-}else{
-    console.log("Email non trovata");
-}
 
 // if(userEmail == emailArr){
 //     console.log("Email trovata");
